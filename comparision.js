5b13c32c0b24390f7252486e963f0c0a7ec22267
ashupanwar10 <panwar.ashish10@gmail.com>
2024-04-18 21:22:18 +0530

    chore: conversion

---
+++ comparision.js
@@ -0,0 +1,3 @@
+// Differences between == and === operators
+console.log(5 == "5"); // true
+console.log(5 === "5"); // false
---
+++ conversionOperation.js
@@ -0,0 +1,26 @@
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+//Operations in JavaScript
+console.log(3 + 4);
+console.log(3 - 4);
+console.log(3 * 4);
+console.log(3 / 4);
+console.log(3 % 4);
+console.log(3 ** 4);
+
+let string1 = "Hello";
+let string2 = "Ashish";
+
+let string3 = string1 + string2;
+console.log(string3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+console.log(((3 + 4) * 5) % 3);
+console.log(3 + ((4 * 5) % 3));
