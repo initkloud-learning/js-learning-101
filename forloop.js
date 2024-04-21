d6a4725fe0036366fe95da3c93524cf6c01818b5
ashupanwar10 <panwar.ashish10@gmail.com>
2024-04-22 00:45:56 +0530

    chore: for - loop

---
+++ forloop.js
@@ -0,0 +1,16 @@
+// for loop in JavaScript
+// for (initialization; condition; increment/decrement) {
+//   // code to be executed
+// }
+
+for (let index = 0; index < 10; index++) {
+  const element = index;
+  console.log(`Index: ${element}`);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(`Index: ${i}`);
+  if (i == 5) {
+    console.log("5 is a lucky number!");
+  }
+}
